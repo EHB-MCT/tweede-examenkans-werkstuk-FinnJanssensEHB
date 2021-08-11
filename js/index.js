a34b31c7c4343384235752f8dd1105f71f8af71e
FinnJanssensEHB <finn.janssens@student.ehb.be>
2021-08-11 15:46:36 +0200

    Added fetchArticles function

--- js/index.js
+++ js/index.js
@@ -4,6 +4,15 @@ import Article from './article.js';
 
 const news = {
   init() {
-
+    this.fetchArticles().then(data => {
+      console.log(data.news);
+    });
+  },
+  async fetchArticles() {
+    const response = await fetch('https://thecrew.cc/news/read.php');
+    const data = await response.json();
+    return await data;
   }
 };
+
+news.init();
